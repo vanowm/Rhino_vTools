2fd297985c43a34ce1dffe46bb27d234f5c6aea0
V@no <git@vano.org>
2026-04-24 10:35:16 -0400

    Select closest center-crossing end caps

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Rhino.PlugIns;
 [assembly: AssemblyProduct("vTools")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2607512e-a1fc-4cf9-9329-a293431437a0")]
-[assembly: AssemblyVersion("26.4.24.1023")]
-[assembly: AssemblyFileVersion("26.4.24.1023")]
-[assembly: AssemblyInformationalVersion("26.04.24.102339")]
+[assembly: AssemblyVersion("26.4.24.1034")]
+[assembly: AssemblyFileVersion("26.4.24.1034")]
+[assembly: AssemblyInformationalVersion("26.04.24.103421")]
 
 [assembly: PlugInDescription(DescriptionType.Address, "")]
 [assembly: PlugInDescription(DescriptionType.Country, "")]
