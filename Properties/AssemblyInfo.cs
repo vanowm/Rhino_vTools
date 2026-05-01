f57b02eac0234e2826cde8e0c89a6269661bb451
V@no <git@vano.org>
2026-04-30 20:05:15 -0400

    ﻿26.04.30.201912: fix Uzip command behavior

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Rhino.PlugIns;
 [assembly: AssemblyProduct("vTools")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2607512e-a1fc-4cf9-9329-a293431437a0")]
-[assembly: AssemblyVersion("26.4.30.2054")]
-[assembly: AssemblyFileVersion("26.4.30.2054")]
-[assembly: AssemblyInformationalVersion("26.04.29.180220")]
+[assembly: AssemblyVersion("26.4.30.2019")]
+[assembly: AssemblyFileVersion("26.4.30.2019")]
+[assembly: AssemblyInformationalVersion("26.04.30.201912")]
 
 [assembly: PlugInDescription(DescriptionType.Address, "")]
 [assembly: PlugInDescription(DescriptionType.Country, "")]
