00a40e5ff3230e29e8f2d8ea8caaee28f3a35b0e
V@no <git@vano.org>
2026-05-23 00:01:36 -0400

    26.5.23.001: vBiminiParts: all params configurable via vTools.config.json; pipe size groups (7/8, 1, 1-1/4, 1-1/2) each with mainPktDepth/secPktDepth; extra rectangle for 1-1/2 inch pipe; PipeSize option list replaces binary toggle

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using Rhino.PlugIns;
 [assembly: AssemblyProduct("vTools")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2607512e-a1fc-4cf9-9329-a293431437a0")]
-[assembly: AssemblyVersion("26.5.22.2320")]
+[assembly: AssemblyVersion("26.5.23.001")]
 [assembly: AssemblyFileVersion("26.5.22.2320")]
 [assembly: AssemblyInformationalVersion("26.5.22.2320")]
 
