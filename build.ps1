76d2792ad34e0f387df54b8ec9b53ab3acff3158
V@no <git@vano.org>
2026-06-09 17:51:38 -0400

    26.6.9.1751: build: sync version metadata

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 Set-Location d:\github\rhino\vTools
 
-# Bump CalVer in csproj and AssemblyInfo: YY.M.D.Hmm (no leading zeros)
+# Bump CalVer in csproj and AssemblyInfo: YY.M.D.Hmm (per memory rules)
 $cur = (Select-String -Path vTools.csproj -Pattern '<Version>([^<]+)').Matches[0].Groups[1].Value
 $v   = Get-Date -Format 'yy.M.d.Hmm'
 (Get-Content vTools.csproj)             -replace [regex]::Escape($cur), $v | Set-Content vTools.csproj
